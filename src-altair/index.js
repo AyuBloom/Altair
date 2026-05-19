c11bb7cd260c177bc364bd68cd0cd3b4b0bf3801
AyuBloom <85625843+AyuBloom@users.noreply.github.com>
2026-05-19 12:50:15 +0700

    script override example

--- src-altair/index.js
+++ src-altair/index.js
@@ -1,3 +1,56 @@
 import "styles/index.js";
+import overrides from "./overrides.js";
 
-console.log("hello");
+/* overriding the original components with additional functions
+ * special methods:
+ * - __altairInit: self-explanatory
+ * - __altairBind: only runs on script, use this to remove old bindings 
+ */
+for (const [original, override] of overrides) {
+    let init;
+    for (const key of Object.getOwnPropertyNames(override.prototype)) {
+        if (key === "constructor") continue;
+
+        if (key === "__altairInit") {
+            init = override.prototype[key];
+        } else {
+            original[key] = override.prototype[key];
+        };
+
+    }
+    init.call(original);
+}
+
+/* cute console msg :) */
+
+setTimeout(() => {
+    console.clear();
+
+    let img = new Image();
+    img.crossOrigin = "anonymous";
+
+    img.onload = () => {
+        const c = document.createElement('canvas');
+        const ctx = c.getContext('2d');
+        if (ctx) {
+            c.width = img.width;
+            c.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            const dataUri = c.toDataURL('image/png');
+            const style = `
+            display: block;
+            background-image: url(${dataUri});
+            background-size: contain;
+            background-repeat: no-repeat;
+            padding: 0 200px;
+            margin: 20px 0 -20px;
+            line-height: 200px;
+        `;
+            console.log('%c ', style);
+            console.log("For feedback, please go to https://github.com/AyuBloom/Altair/issues")
+        }
+    };
+
+    img.src = "https://raw.githubusercontent.com/AyuBloom/Altair/refs/heads/main/assets/altair_banner.png";
+}, 1000);
---
+++ src-altair/overrides.js
@@ -0,0 +1,7 @@
+import UiChatOverride from "./overrides/UiChat";
+
+const overrides = [
+    [game.ui.components.Chat, UiChatOverride],
+];
+
+export default overrides;
